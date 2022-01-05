5b1dd2cb1026b1f365156e80030eab059dd5cb96
TauH <thaywood988@gmail.com>
2022-01-05 23:37:10 +1100

    more commits

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,12 +15,28 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const postDb = async (content) => {
   console.log("POST to the database");
+  console.log(content);
   const jateDb = await openDB("jate", 1);
+  console.log(jateDb);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
+  console.log(store);
   const request = store.add({ content });
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  console.log("🚀 - data post saved to the database", result);
+};
+
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  console.log(content);
+  const jateDb = await openDB("jate", 1);
+  console.log(jateDb);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  console.log(store);
+  const request = store.put({ id: 1, content });
+  const result = await request;
+  console.log("🚀 - data put saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
